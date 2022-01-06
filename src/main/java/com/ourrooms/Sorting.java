909019712168effcacaafec683e74b406da867ea
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-06 16:48:37 +0700

    Fix compile error

--- src/main/java/com/ourrooms/Sorting.java
+++ src/main/java/com/ourrooms/Sorting.java
@@ -8,16 +8,6 @@ public class Sorting {
         printArray(array);
     }
 
-    public static void (int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j+1]) {
-                    swap(array, j, j+1);
-                }
-            }
-        }
-    }
-
     public static void selectionSort(int[] array) {
         int minIndex;
 
