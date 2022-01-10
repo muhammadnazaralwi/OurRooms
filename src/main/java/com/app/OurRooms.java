67456ad89283a008aad6ed8cf3ff3560a97043ef
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-10 12:44:28 +0700

    Add composition root for composing other component

---
+++ src/main/java/com/app/OurRooms.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import com.ourrooms.User;
+
+import java.util.Scanner;
+
+public class OurRooms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nin = scanner.nextLine();
+        String name = scanner.nextLine();
+        User user = new User(nin, name);
+    }
+}
