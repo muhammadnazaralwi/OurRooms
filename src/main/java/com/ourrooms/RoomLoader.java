61d2c86babf98247223aea438c261f28a859a922
Nazar Alwi <alwinazar75@gmail.com>
2022-01-10 08:47:53 +0700

    Load room list

---
+++ src/main/java/com/ourrooms/RoomLoader.java
@@ -0,0 +1,13 @@
+package com.ourrooms;
+
+import java.net.URL;
+
+public class RoomLoader {
+    public void load() {
+        System.out.println("1. Deluxe Double Room ");
+        System.out.println("2. Deluxe Twin Room ");
+        System.out.println("3. Grand Deluxe ");
+        System.out.println("4. Suite Junior ");
+        System.out.println("5. Suite Executive ");
+    }
+}
