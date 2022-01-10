0252bb33380f19126013efbfa45ec9a30fd1856d
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-10 12:39:15 +0700

    Add `User` model

---
+++ src/main/java/com/ourrooms/User.java
@@ -0,0 +1,27 @@
+package com.ourrooms;
+
+public class User {
+    String nin;
+    String name;
+
+    public User(String nin, String name) {
+        this.nin = nin;
+        this.name = name;
+    }
+
+    public String getNin() {
+        return nin;
+    }
+
+    public void setNin(String nin) {
+        this.nin = nin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
