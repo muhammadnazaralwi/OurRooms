19d95f7238e148711bfb041ff1e36be923504413
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-01-11 13:24:46 +0700

    Add constructor, getter, and setter Room

--- src/main/java/room/Room.java
+++ src/main/java/room/Room.java
@@ -6,4 +6,52 @@ public class Room {
     String[] facilities;
     int price;
     boolean isFreeBreakfast;
+
+    public Room(int id, String name, String[] facilities, int price, boolean isFreeBreakfast) {
+        this.id = id;
+        this.name = name;
+        this.facilities = facilities;
+        this.price = price;
+        this.isFreeBreakfast = isFreeBreakfast;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getFacilities() {
+        return facilities;
+    }
+
+    public void setFacilities(String[] facilities) {
+        this.facilities = facilities;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isFreeBreakfast() {
+        return isFreeBreakfast;
+    }
+
+    public void setFreeBreakfast(boolean freeBreakfast) {
+        isFreeBreakfast = freeBreakfast;
+    }
 }
